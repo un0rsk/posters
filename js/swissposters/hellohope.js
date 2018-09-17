5520f591ebee7ea68753f30f56495919f5827e0d
andrii <andrii.uvarov@nav.no>
2018-09-17 09:08:31 +0200

    init Hello Hope poster

---
+++ js/swissposters/hellohope.js
@@ -0,0 +1,14 @@
+class HelloHope extends React.Component {
+  render() {
+    return (
+      <Poster>
+        <div className='HelloHope'>
+          <div className='Header'>hello hope</div>
+        </div>
+        <div className='Rays'>
+        
+        </div>
+      </Poster>
+    )
+  }
+}
