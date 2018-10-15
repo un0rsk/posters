7033c706d54f6eb99503a44d7705c9afb297f160
andrii <andrii.uvarov@nav.no>
2018-10-15 15:05:18 +0200

    Updates

---
+++ js/swissposters/hellohope.js
@@ -0,0 +1,15 @@
+// https://codepen.io/pestbarn/pen/aBybeK
+class HelloHope extends React.Component {
+  render() {
+    return (
+      <Poster>
+        <div className='HelloHope'>
+          <div className='Header'>hello hope</div>
+        </div>
+        <div className='Rays'>
+
+        </div>
+      </Poster>
+    )
+  }
+}
